f0c1b83b130ee8da56c820bfefc5a79cd0d52c9b
Derek <su.derek@gmail.com>
2025-05-17 00:06:15 +0800

    fix `callbck` in init.lua

--- neovim-pde/init.lua
+++ neovim-pde/init.lua
@@ -5,7 +5,7 @@ if vim.fn.argc(-1) == 0 then
   vim.api.nvim_create_autocmd("User", {
     group = vim.api.nvim_create_augroup("NeovimPDE", { clear = true }),
     pattern = "VeryLazy",
-    callbck = function()
+    callback = function()
       require "config.autocmds"
       require "config.keymaps"
     end,
